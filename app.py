61200277f4264e3267298786bfb3bea11dbf1609
Forer <774714620@qq.com>
2020-05-24 11:11:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,15 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-
+count = 0
 @app.route('/')
 def hello_world():
     return 'Hello ace girl'
+
+@app.route('/count')
+def count_test():
+    count = count + 1
+    return f"Hello ace girl, {count}"
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=8080)
